0ef6d0cf30b414aed406bfd409b372042a0b9ff0
dcocherell <dcocherell@choctawnation.com>
2023-07-19 13:44:22 -0500

    Fixed Paths.

--- chryblk/urls.py
+++ chryblk/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # Import views from current directory
 
 urlpatterns = [
-    path('user_input/', views.user_input, name='user_input'),  # Define your custom route
+    path('', views.user_input, name='user_input'),  # Define your custom route
 ]
