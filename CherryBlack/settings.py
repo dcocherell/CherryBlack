92ad02672d1624939fded9704b142e75a39a3183
dcocherell <dcocherell@choctawnation.com>
2023-07-19 13:22:37 -0500

    Added Unit and Integration tests to the suite.

--- CherryBlack/settings.py
+++ CherryBlack/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o1w$-yal^ze0harz*mj#n2wu94osjv2v1az%n*&!-%3)mik4fh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
