36840d2fd9d2f191ca977ae57da881a6f8067ae5
dcocherell <dcocherell@choctawnation.com>
2023-07-20 10:54:44 -0500

    Created Unit and Integration tests for input data and report data.

--- chryblk/test/test_integration.py
+++ chryblk/test/test_integration.py
@@ -1,16 +1,36 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+from chryblk.models import FinancialData
+import datetime
 
-class IntegrationTest(TestCase):
+class UserFlowTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.data = {
+            'username': 'testuser',
+            'password': '12345'
+        }
+        self.fin_data = {
+            'date': datetime.date.today(),
+            'income': 5000.00,
+            'expense': 2000.00,
+            'expense_type': 'Rent',
+            'state': 'CA'
+        }
 
-    def test_form_submission_flow(self):
-        # Test GET request
-        response = self.client.get(reverse('user_input'))
-        self.assertEqual(response.status_code, 200)
+    def test_user_flow(self):
+        # User login
+        response = self.client.post(reverse('chryblk:login'), self.data)
+        self.assertEqual(response.status_code, 302)  # Assuming it redirects after login
 
-        # Test POST request
-        response = self.client.post(reverse('user_input'), {'text_input': 'Integration test'})
+        # Input financial data
+        response = self.client.post(reverse('chryblk:input_financial_data'), self.fin_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(FinancialData.objects.filter(income=5000).exists())
+
+        # Accessing the report page
+        response = self.client.get(reverse('chryblk:report'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Integration test', response.content.decode())
+        self.assertContains(response, '5000')
--- chryblk/test/test_unit.py
+++ chryblk/test/test_unit.py
@@ -1,17 +1,36 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+from chryblk.models import FinancialData
+import datetime
 
-class UserInputTest(TestCase):
+class InputFinancialDataTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        self.data = {
+            'date': datetime.date.today(),
+            'income': 5000.00,
+            'expense': 2000.00,
+            'expense_type': 'Rent',
+            'state': 'CA'
+        }
 
-    def test_user_input_GET(self):
-        # Test GET request
-        response = self.client.get(reverse('user_input'))
-        self.assertEqual(response.status_code, 200)
+    def test_input_financial_data(self):
+        response = self.client.post(reverse('chryblk:input_financial_data'), self.data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(FinancialData.objects.filter(income=5000.00).exists())
 
-    def test_user_input_POST(self):
-        # Test POST request
-        response = self.client.post(reverse('user_input'), {'text_input': 'Test string'})
+
+class ReportTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        FinancialData.objects.create(user=self.user, date=datetime.date.today(), income=5000, expense=2000.00, expense_type='Rent', state='CA')
+
+    def test_report(self):
+        response = self.client.get(reverse('chryblk:report'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Test string', response.content.decode())
+        self.assertContains(response, '5000')
