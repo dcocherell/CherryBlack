497cf69ad4b2716f989c42c836c1feef9ff0fd71
dcocherell <dcocherell@choctawnation.com>
2023-07-20 11:09:36 -0500

    renamed url

--- chryblk/urls.py
+++ chryblk/urls.py
@@ -5,7 +5,7 @@ app_name = 'chryblk'
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('input_financial_data/', views.input_financial_data, name='input_financial_data'),
     path('report/', views.report, name='report'),
