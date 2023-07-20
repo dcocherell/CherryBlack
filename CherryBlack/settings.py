3c0bfe3ba3396a9f554ea5d69126409dee7c468c
dcocherell <dcocherell@choctawnation.com>
2023-07-20 11:03:29 -0500

    Potentially didn't fix the redirect issue last time.

--- CherryBlack/settings.py
+++ CherryBlack/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = 'chryblk:home'
+#redirect to home after account activation
+
--- chryblk/views.py
+++ chryblk/views.py
@@ -16,7 +16,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('chryblk:home.html')
+            return redirect('chryblk/home.html')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
