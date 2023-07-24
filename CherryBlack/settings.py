47ca8c0a7b1f13b205ba2fc3075e5f57bb376e47
dcocherell <dcocherell@choctawnation.com>
2023-07-24 09:35:37 -0500

    Added redis config to settings.py

--- CherryBlack/settings.py
+++ CherryBlack/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,9 +124,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'chryblk:home'
 #redirect to home after account activation
 
+REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+
 # Celery Configuration Options
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'REDIS_URL'
+CELERY_RESULT_BACKEND = 'REDIS_URL'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
